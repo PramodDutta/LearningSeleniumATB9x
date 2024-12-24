e056c44aa24650888c889e6db2d36bbb9d29edd0
Promode <pramoddutta_96@yahoo.com>
2024-12-24 06:53:51 +0530

    24th Dec

---
+++ src/test/java/com/thetestingacademy/ex01_SeleniumBasics/TestSelenium02.java
@@ -0,0 +1,29 @@
+package com.thetestingacademy.ex01_SeleniumBasics;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium02 {
+
+    @Description("Open the App.vwo.com and Get the title")
+    @Test
+    public void test_Selenium01(){
+
+        // Open a URL
+        // Print the title
+        EdgeDriver driver = new EdgeDriver();
+
+        // new EdgeDriver(); -> POST Request (localhost:56055), Create Session Endpoint
+        // Edge Real Browser
+
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getTitle());
+
+
+
+
+    }
+
+}
