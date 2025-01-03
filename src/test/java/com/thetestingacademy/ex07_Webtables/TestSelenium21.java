99c19a7882600dd45fac5500d86c536a58cdf6d3
Promode <pramoddutta_96@yahoo.com>
2025-01-03 06:35:28 +0530

    3rd Dec

--- src/test/java/com/thetestingacademy/ex07_Webtables/TestSelenium21.java
+++ src/test/java/com/thetestingacademy/ex07_Webtables/TestSelenium21.java
@@ -21,6 +21,7 @@ public class TestSelenium21 {
         driver.get("https://awesomeqa.com/webtable.html");
         driver.manage().window().maximize();
 
+
         // xpath ->  //table[@id="customers"]/tbody/tr[5]/td[2]
 
 
@@ -34,6 +35,29 @@ public class TestSelenium21 {
         String first_part = "//table[@id=\"customers\"]/tbody/tr[";
         String second_part = "]/td[";
         String third_part = "]";
+
+        int row = driver.findElements(By.xpath("//table[@id=\"customers\"]/tbody/tr")).size();
+        int col = driver.findElements(By.xpath("//table[@id=\"customers\"]/tbody/tr[2]/td")).size();
+
+        for (int i=2;i<=row;i++){
+            for(int j=1;j<=col;j++){
+                String dynamic_path = first_part+i+second_part+j+third_part;
+//                System.out.println(dynamic_path);
+                String data = driver.findElement(By.xpath(dynamic_path)).getText();
+//                System.out.println(data);
+                if(data.contains("Helen Bennett")){
+                    String country_path = dynamic_path+"/following-sibling::td";
+                    String country_text = driver.findElement(By.xpath(country_path)).getText();
+                    System.out.println("------");
+                    System.out.println("Helen Bennett is In - " + country_text);
+
+                }
+
+
+            }
+        }
+
+
 
 
 
